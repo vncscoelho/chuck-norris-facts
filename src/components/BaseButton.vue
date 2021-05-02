03c1018701608b0f9c026df75e44bff922cb81fb
Vinicius <vncs.coelho@gmail.com>
2021-05-02 11:12:14 -0300

    feat: add base button component

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,47 @@
+<template>
+  <button
+    class="base-button"
+    :class="{ 'base-button--primary': primary }"
+    @click="$emit('click')"
+  >
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    primary: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.base-button {
+  padding: 1em 2em;
+  font-size: 1.25em;
+  font-weight: bolder;
+  text-transform: uppercase;
+  color: #fff;
+  border: 0;
+  background: none;
+  transition: 150ms ease-in;
+  transition-property: color, background-color;
+
+  &:hover {
+    background: lighten($secondary, 15);
+  }
+
+  &--primary {
+    background: $primary;
+    color: #fff;
+
+    &:hover {
+      background: lighten($primary, 15);
+    }
+  }
+}
+</style>
