bebc8a66234a56319ceca1ec0db2add878f955e0
Vinicius <vncs.coelho@gmail.com>
2021-05-02 11:12:07 -0300

    feat: add search box component

---
+++ src/components/SearchBox.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="search-box">
+    <input
+      type="text"
+      class="search-box__input"
+      placeholder="Search for a fact or keyword here"
+    />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.search-box {
+  &__input {
+    width: 100%;
+    padding: 2rem;
+    border: 0;
+    background: transparent;
+    color: #fff;
+    font-family: "Work Sans";
+    font-size: 1.25em;
+  }
+}
+</style>
