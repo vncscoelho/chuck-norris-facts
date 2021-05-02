3a566ca888e3249c7f6055cef120241ccee763d6
Vinicius <vncs.coelho@gmail.com>
2021-05-02 13:50:39 -0300

    chore: update example files

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,24 @@
 <template>
   <div class="home">
     <page-header />
+    <div class="container">
+      <fact-card>
+        Fort Knox wanted to store gold in Chuck Norris' beard - but Chuck
+        Norris' beard makes gold rust
+      </fact-card>
+    </div>
   </div>
 </template>
 
 <script>
 import PageHeader from "@/components/PageHeader.vue";
+import FactCard from "@/components/FactCard.vue";
 
 export default {
   name: "Home",
   components: {
     PageHeader,
+    FactCard,
   },
 };
 </script>
